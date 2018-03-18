69d1be1dde0742794c0a88b40de6de5a0c9d753e
Andrey Minev <andrey_st_minev@yahoo.com>
2018-03-18 20:05:29 +0200

    added 4

---
+++ intro-and-basic-syntax/exercise/Program4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace solution4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugarContent = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}ml {1}:", volume, name);
+            Console.WriteLine("{0}kcal, {1}g sugars", (volume / 100.0) * energyContent, (volume / 100.0) * sugarContent);
+        }
+    }
+}
