75238048a34d590c1f53c3fa7458fba938b7a94d
tedoo <32912265+ssaxis@users.noreply.github.com>
2018-03-18 20:45:42 +0200

    documented the code

--- intro-and-basic-syntax/exercise/Program-two.cs
+++ intro-and-basic-syntax/exercise/Program-two.cs
@@ -6,10 +6,13 @@ namespace Problem_two
     {
         static void Main(string[] args)
         {
-
+			//program receives width input from the user
             var width = double.Parse(Console.ReadLine());
+			//program receives height input from the user
             var height = double.Parse(Console.ReadLine());
+			//here we calculate the area using the input from the user
             var area = width * height;
+			//finally print the area result on the console
             Console.WriteLine($"{area:f2}");
 
         }
