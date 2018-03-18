2fbd7e5120939837d5bb5255be37e82a289ce6d5
tedoo <32912265+ssaxis@users.noreply.github.com>
2018-03-18 19:14:21 +0200

    added problem-five.cs

---
+++ intro-and-basic-syntax/exercise/problem-five.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var currentHealth = int.Parse(Console.ReadLine());
+            var maxHealth = int.Parse(Console.ReadLine());
+            var currentEnergy = int.Parse(Console.ReadLine());
+            var maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.Write("Health: |");
+            for (int i = 1; i <= currentHealth; i++)
+            {
+                Console.Write("|");
+            }
+            for (int i = 1; i <= maxHealth - currentHealth; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write("|");
+            Console.WriteLine();
+
+            Console.Write("Energy: |");
+            for (int i = 1; i <= currentEnergy; i++)
+            {
+                Console.Write("|");
+            }
+            for (int i = 1; i <= maxEnergy - currentEnergy; i++)
+            {
+                Console.Write(".");
+            }
+            Console.Write("|");
+
+        }
+    }
+}
