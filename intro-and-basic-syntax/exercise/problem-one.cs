8775cdb6a77e67f8ac76f808671b2a208c8136c7
tedoo <32912265+ssaxis@users.noreply.github.com>
2018-03-18 18:54:49 +0200

    added problem-one.cs

---
+++ intro-and-basic-syntax/exercise/problem-one.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = int.Parse(Console.ReadLine());
+            var second = int.Parse(Console.ReadLine());
+            var third = int.Parse(Console.ReadLine());
+            var fourth = int.Parse(Console.ReadLine());
+
+            Console.Write($"{first:D4} {second:D4} {third:D4} {fourth:D4}");
+
+        }
+    }
+}
