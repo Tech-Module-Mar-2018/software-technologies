6f060289ca223c6f952c291f65bfe0225c47e8f2
Andrey Minev <andrey_st_minev@yahoo.com>
2018-03-19 10:34:58 +0200

    changed variable in problem 2

--- intro-and-basic-syntax/exercise/Program-two.cs
+++ intro-and-basic-syntax/exercise/Program-two.cs
@@ -7,11 +7,11 @@ namespace Problem_two
         static void Main(string[] args)
         {
 			//program receives width input from the user
-            var width = double.Parse(Console.ReadLine());
+            double widthDouble = double.Parse(Console.ReadLine()); //changed variable
 			//program receives height input from the user
             var height = double.Parse(Console.ReadLine());
 			//here we calculate the area using the input given from the user
-            var area = width * height;
+            var area = widthDouble * height; //changed variable
 			//finally print the area result on the console
             Console.WriteLine($"{area:f2}");
 
