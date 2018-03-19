c47f5fdb0949bacf23ced94e4297753a0d086e17
ralijordanova <rali_v666@abv.bg>
2018-03-19 18:43:19 +0200

    add comment in code

--- intro-and-basic-syntax/exercise/problem-four.cs
+++ intro-and-basic-syntax/exercise/problem-four.cs
@@ -6,11 +6,13 @@ namespace solution4
     {
         static void Main(string[] args)
         {
+            //read food product
             string name = Console.ReadLine();
             int volume = int.Parse(Console.ReadLine());
             int energyContent = int.Parse(Console.ReadLine());
             int sugarContent = int.Parse(Console.ReadLine());
 
+            //calculate the energy and sugar content for the given volume
             Console.WriteLine("{0}ml {1}:", volume, name);
             Console.WriteLine("{0}kcal, {1}g sugars", (volume / 100.0) * energyContent, (volume / 100.0) * sugarContent);
         }
