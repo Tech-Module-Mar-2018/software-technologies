4a2d96a29a0c91bc86c0737920b7a626dd2e1447
ralijordanova <rali_v666@abv.bg>
2018-03-19 18:30:36 +0200

    Added problem-three.cs

---
+++ intro-and-basic-syntax/exercise/problem-three.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace problem_three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kilometers = double.Parse(Console.ReadLine());
+            var mile = 1.60934 * kilometers;
+            Console.WriteLine($"{mile:f2}");
+        }
+    }
+}
