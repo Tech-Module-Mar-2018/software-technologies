d1954b2e7915c4adc07827fdf2e7499d88bf90cb
ivanborisovg <ivanborisovg@gmail.com>
2018-03-20 19:45:09 +0200

    Added commends on the code of problem-three.cs

--- intro-and-basic-syntax/exercise/problem-three.cs
+++ intro-and-basic-syntax/exercise/problem-three.cs
@@ -1,14 +1,15 @@
-﻿//CONFIRMED from warum
-using System;
+﻿using System;
 
 namespace problem_three
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {   // Read and parse to floating point the current input
             var kilometers = double.Parse(Console.ReadLine());
+            // Convert the kilometers to miles
             var mile = 1.60934 * kilometers;
+            //Print the input
             Console.WriteLine($"{mile:f2}");
         }
     }
