52fab8c48868e21aed65c60cdceb7b1c0153b7fb
tedoo <32912265+ssaxis@users.noreply.github.com>
2018-03-21 20:34:41 +0200

    commented on problem-one.cs

--- intro-and-basic-syntax/exercise/problem-one.cs
+++ intro-and-basic-syntax/exercise/problem-one.cs
@@ -7,11 +7,15 @@ namespace Problem1
     {
         static void Main(string[] args)
         {
+			//input first part of card digits
             var first = int.Parse(Console.ReadLine());
+			//input second part of card digits
             var second = int.Parse(Console.ReadLine());
+			//input third part of card digits
             var third = int.Parse(Console.ReadLine());
+			//input fourth part of card digits
             var fourth = int.Parse(Console.ReadLine());
-
+			//print result with leading zeroes if needed
             Console.Write($"{first:D4} {second:D4} {third:D4} {fourth:D4}");
 
         }
