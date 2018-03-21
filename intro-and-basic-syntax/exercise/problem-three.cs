a35b3e8f38d31815c8ad84ae63bfe9ac108ecfa6
tedoo <32912265+ssaxis@users.noreply.github.com>
2018-03-21 20:44:45 +0200

    changed variable name in problem-three.cs

--- intro-and-basic-syntax/exercise/problem-three.cs
+++ intro-and-basic-syntax/exercise/problem-three.cs
@@ -9,9 +9,9 @@ namespace problem_three
         {   // Read and parse to floating point the current input
             var kilometers = double.Parse(Console.ReadLine());
             // Convert the kilometers to miles
-            var mile = 1.60934 * kilometers;
+            var miles = 1.60934 * kilometers;
             //Print the input
-            Console.WriteLine($"{mile:f2}");
+            Console.WriteLine($"{miles:f2}");
         }
     }
 }
