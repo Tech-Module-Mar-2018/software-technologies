7236192318434f0a98025ee90f51675c94f691c7
ralijordanova <rali_v666@abv.bg>
2018-03-22 18:02:29 +0200

    rename in problem-one.cs

--- intro-and-basic-syntax/exercise/problem-one.cs
+++ intro-and-basic-syntax/exercise/problem-one.cs
@@ -8,15 +8,15 @@ namespace Problem1
         static void Main(string[] args)
         {
 			//input first part of card digits
-            var first = int.Parse(Console.ReadLine());
+            var theFirst = int.Parse(Console.ReadLine());
 			//input second part of card digits
-            var second = int.Parse(Console.ReadLine());
+            var theSecond = int.Parse(Console.ReadLine());
 			//input third part of card digits
-            var third = int.Parse(Console.ReadLine());
+            var theThird = int.Parse(Console.ReadLine());
 			//input fourth part of card digits
-            var fourth = int.Parse(Console.ReadLine());
+            var theFourth = int.Parse(Console.ReadLine());
 			//print result with leading zeroes if needed
-            Console.Write($"{first:D4} {second:D4} {third:D4} {fourth:D4}");
+            Console.Write($"{theFirst:D4} {theSecond:D4} {theThird:D4} {theFourth:D4}");
 
         }
     }
